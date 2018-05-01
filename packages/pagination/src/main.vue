6038c63bf6bf0370c11bfcbb9db86659f3d1cdd9
Leona Yang <leonayang@sf-express.com>
2018-05-01 22:43:54 +0800

    add pagination component

--- packages/pagination/src/main.vue
+++ packages/pagination/src/main.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="sofa-pagination">
-    pagination
-  </div>
+  <ul @click="onPagerClick" class="sofa-pagination">
+    <li
+      :class="{ active: currentPage === 1, disabled }"
+      v-if="pageCount > 0"
+      class="number">1</li>
+    <li
+      class="sofa-icon more btn-quickprev"
+      :class="[quickprevIconClass, { disabled }]"
+      v-if="showPrevMore"
+      @mouseenter="onMouseenter('left')"
+      @mouseleave="quickprevIconClass = 'sofa-icon-more'"></li>
+    <li
+      v-for="pager in pagers"
+      :key="pager"
+      :class="{ active: currentPage === pager, disabled }"
+      class="number">{{ pager }}</li>
+    <li
+      class="sofa-icon more btn-quicknext"
+      :class="[quicknextIconClass, { disabled }]"
+      v-if="showNextMore"
+      @mouseenter="onMouseenter('right')"
+      @mouseleave="quicknextIconClass = 'sofa-icon-more'"></li>
+    <li
+      :class="{ active: currentPage === pageCount, disabled }"
+      class="number"
+      v-if="pageCount > 1">{{ pageCount }}</li>
+  </ul>
 </template>
 <script>
 export default {
@@ -20,21 +44,128 @@ export default {
   componentName: 'SofaPagination',
 
   props: {
-    value: {},
-    label: {},
+    currentPage: Number,
+
+    pageCount: Number,
+
+    pagerCount: Number,
+
     disabled: Boolean,
-    name: String,
+  },
+
+  watch: {
+    showPrevMore(val) {
+      if (!val) this.quickprevIconClass = 'sofa-icon-more';
+    },
+
+    showNextMore(val) {
+      if (!val) this.quicknextIconClass = 'sofa-icon-more';
+    },
   },
 
   data() {
     return {
-      focus: false,
+      current: null,
+      showPrevMore: false,
+      showNextMore: false,
+      quicknextIconClass: 'sofa-icon-more',
+      quickprevIconClass: 'sofa-icon-more',
     };
   },
   computed: {
+    pagers() {
+      const pagerCount = this.pagerCount;
+      const halfPagerCount = (pagerCount - 1) / 2;
+
+      const currentPage = Number(this.currentPage);
+      const pageCount = Number(this.pageCount);
+
+      let showPrevMore = false;
+      let showNextMore = false;
+
+      if (pageCount > pagerCount) {
+        if (currentPage > pagerCount - halfPagerCount) {
+          showPrevMore = true;
+        }
+
+        if (currentPage < pageCount - halfPagerCount) {
+          showNextMore = true;
+        }
+      }
+
+      const array = [];
+
+      if (showPrevMore && !showNextMore) {
+        const startPage = pageCount - (pagerCount - 2);
+        for (let i = startPage; i < pageCount; i += 1) {
+          array.push(i);
+        }
+      } else if (!showPrevMore && showNextMore) {
+        for (let i = 2; i < pagerCount; i += 1) {
+          array.push(i);
+        }
+      } else if (showPrevMore && showNextMore) {
+        const offset = Math.floor(pagerCount / 2) - 1;
+        for (let i = currentPage - offset; i <= currentPage + offset; i += 1) {
+          array.push(i);
+        }
+      } else {
+        for (let i = 2; i < pageCount; i += 1) {
+          array.push(i);
+        }
+      }
+      this.showPrevMore = showPrevMore;
+      this.showNextMore = showNextMore;
+
+      return array;
+    },
   },
 
   methods: {
+    onPagerClick(event) {
+      console.log('onPagerClick', event);
+      const target = event.target;
+      if (target.tagName === 'UL' || this.disabled) {
+        return;
+      }
+
+      let newPage = Number(event.target.textContent);
+      const pageCount = this.pageCount;
+      const currentPage = this.currentPage;
+      const pagerCountOffset = this.pagerCount - 2;
+
+      if (target.className.indexOf('more') !== -1) {
+        if (target.className.indexOf('quickprev') !== -1) {
+          newPage = currentPage - pagerCountOffset;
+        } else if (target.className.indexOf('quicknext') !== -1) {
+          newPage = currentPage + pagerCountOffset;
+        }
+      }
+
+      /* istanbul ignore if */
+      if (!isNaN(newPage)) {
+        if (newPage < 1) {
+          newPage = 1;
+        }
+
+        if (newPage > pageCount) {
+          newPage = pageCount;
+        }
+      }
+
+      if (newPage !== currentPage) {
+        this.$emit('change', newPage);
+      }
+    },
+
+    onMouseenter(direction) {
+      if (this.disabled) return;
+      if (direction === 'left') {
+        this.quickprevIconClass = 'sofa-icon-d-arrow-left';
+      } else {
+        this.quicknextIconClass = 'sofa-icon-d-arrow-right';
+      }
+    },
   },
 };
 </script>
--- site/src/pages/Pagination/index.vue
+++ site/src/pages/Pagination/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <comp-show-container>
-      <sofa-pagination></sofa-pagination>
+      <sofa-pagination :currentPage="1" :pageCount="100" :pagerCount="5"></sofa-pagination>
+    </comp-show-container>
+
+    <comp-show-container>
+      <sofa-pagination :currentPage="9" :pageCount="100" :pagerCount="5"></sofa-pagination>
+    </comp-show-container>
+
+    <comp-show-container>
+      <sofa-pagination :currentPage="50" :pageCount="100" :pagerCount="7"></sofa-pagination>
     </comp-show-container>
   </div>
 </template>
